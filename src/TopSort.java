3e5342ce3f2c13182c6f465d4220476aa27c85de
Christian Solis-Shepperson <christian.solisshepperson.15@cnu.edu>
2018-03-12 16:50:22 -0400

    third commit - added TopSort Class with outline

---
+++ src/TopSort.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class TopSort {
+
+	public ArrayList<String> dfsTopSort(Graph g) {
+		long startTime = System.nanoTime();
+		
+		//write code
+		
+		
+
+		long difference = System.nanoTime() - startTime;
+		System.out.println("Elapsed Time: " + difference);
+		
+		return new ArrayList<String>();
+	}
+
+	public void dfs(String vertex, Graph g) {
+
+	}
+
+	public ArrayList<String> sourceTopSort(Graph g) {
+		long startTime = System.nanoTime();
+		
+		//write code
+		
+		
+
+		long difference = System.nanoTime() - startTime;
+		System.out.println("Elapsed Time: " + difference);
+		
+		return new ArrayList<String>();
+	}
+
+}
